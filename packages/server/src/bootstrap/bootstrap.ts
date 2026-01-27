2940d84b6c873d235743aad5e1397e64dc9d0c53
Jake Mc <3658522+startswithaj@users.noreply.github.com>
2026-01-27 13:33:25 +1000

    wires ScheduleService into bootstrap

--- packages/server/src/bootstrap/bootstrap.ts
+++ packages/server/src/bootstrap/bootstrap.ts
@@ -7,11 +7,16 @@ import { VehicleManager } from "../services/VehicleManager.ts";
 import { EnergyAdapterManager } from "../services/EnergyAdapterManager.ts";
 import { EnergyPoller } from "../services/EnergyPoller.ts";
 import { VehicleService } from "../services/VehicleService.ts";
+import { ScheduleService } from "../services/ScheduleService.ts";
 import { DataRecorder } from "../services/DataRecorder.ts";
 import { ChargeController } from "../services/ChargeController.ts";
 import { createAppRouter } from "../trpc/root.ts";
 import type { TrpcContext } from "../trpc/trpc.ts";
 
+  const scheduleService = new ScheduleService(
+    db,
+    new Logger("ScheduleService", logLevel),
+  );
   new DataRecorder(
     db,
     vehicleManager,
