136fe9e4d51adc2b0f499e59a0fb23d018ce93a5
Jake Mc <3658522+startswithaj@users.noreply.github.com>
2026-02-28 11:35:47 +1000

    wires ToastProvider into App

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,7 +1,10 @@
 import { Theme } from "@radix-ui/themes";
+import { ToastProvider } from "./hooks/useToast.tsx";
 function AppContent() {
   return (
     <Theme appearance={appearance}>
+      <ToastProvider>
+      </ToastProvider>
     </Theme>
   );
 }
